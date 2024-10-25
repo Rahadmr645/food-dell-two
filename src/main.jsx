50aa2fc2117e10f8f3525d49b6c0ae9814bdda8e
rahadm645 <rahadmr645@gmail.com>
2024-10-26 02:06:30 +0600

    UPdate mainx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import StoreContextProvider  from "./context/Context.jsx";
 createRoot(document.getElementById("root")).render(
   <Router>
